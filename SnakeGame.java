ea42e53ce9086bf5485aa14966908b88b71b468b
BrotherKyle <50892025+EspinasXC@users.noreply.github.com>
2022-10-22 12:35:30 -0400

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,14 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class SnakeGame {
+    public static void main(String[] args){
+
+        //GameFrame frame = new GameFrame(); 
+        //Since we will never use the frame variable, we can just initialize 
+        new GameFrame();
+    }
+}
